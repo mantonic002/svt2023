64c8988a5686604a6db5349cb486848f0f0ed1f7
mantonic002 <mantonic002@gmail.com>
2023-05-08 22:38:54 +0200

    Added model classes for users, groups, group admins and posts

---
+++ src/main/java/ftn/socialnetwork/model/Group.java
@@ -0,0 +1,35 @@
+package ftn.socialnetwork.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "groups")
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String descripiton;
+
+    @Column(nullable = false)
+    private Date creationDate;
+
+    @Column(nullable = false)
+    private boolean isSuspended;
+
+    @Column
+    private String suspendedReason;
+}
