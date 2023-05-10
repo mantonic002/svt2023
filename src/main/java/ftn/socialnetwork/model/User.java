373937049054117e496708c9db2536ba5bfc8587
mantonic002 <mantonic002@gmail.com>
2023-05-10 16:05:34 +0200

    added connection to a database

--- src/main/java/ftn/socialnetwork/model/User.java
+++ src/main/java/ftn/socialnetwork/model/User.java
@@ -29,7 +29,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(columnDefinition = "TIMESTAMP")
+    @Column
     private LocalDateTime lastLogin;
 
     @Column(nullable = false)
